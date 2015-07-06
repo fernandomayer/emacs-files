c4b121adf846ec875f760613833e8dbd9964792c
Fernando Mayer <fernandomayer@gmail.com>
2015-07-06 20:43:28 -0300

    auto-complete mode and several functions stoled from @walmes

--- emacs.el
+++ emacs.el
@@ -83,6 +83,15 @@
 ; AND bind it to F5
 (global-set-key [f5] 'revert-buffer-no-confirm)
 
+;; M-= to get division line with 70 =.
+(global-set-key [?\M-=] (kbd "C-u 7 0 ="))
+
+;; M-- to get division line with 70 -.
+(global-set-key [?\M--] (kbd "C-u 7 0 -"))
+
+;; C-- to get division line with 43 -.
+(global-set-key [?\C--] (kbd "C-u 4 3 -"))
+
 ;;......................................................................
 ;; DEFUNCT options
 ;;......................................................................
@@ -136,7 +145,7 @@
 ;;......................................................................
 
 ;;======================================================================
-;; markdown mode
+;; (R) markdown mode
 ;;======================================================================
 
 ;; make markdown mode visible. Install from
@@ -177,6 +186,17 @@
 (add-to-list 'auto-mode-alist '("\\.Rcpp" . poly-r+c++-mode))
 (add-to-list 'auto-mode-alist '("\\.cppR" . poly-c++r-mode))
 (provide 'polymode-configuration)
+
+;; Insert a new (empty) chunk to R markdown
+(defun insert-chunk ()
+  "Insert chunk environment Rmd sessions."
+  (interactive)
+  (insert "```{r}\n\n```")
+  (forward-line -1)
+  )
+; key binding
+(global-set-key (kbd "C-c i") 'insert-chunk)
+
 
 ;;======================================================================
 ;; LaTeX (and AUCTeX) customizations
@@ -387,6 +407,236 @@ options(oo)})\n"  string) buf)
 ;(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
 
 ;;......................................................................
+
+;;======================================================================
+;; Auto complete mode
+;;======================================================================
+
+;; auto-complete
+;; instalar https://aur.archlinux.org/packages/auto-complete/
+(add-to-list 'load-path "/usr/share/emacs/site-lisp/auto-complete")
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories
+	     "/usr/share/emacs/site-lisp/auto-complete/ac-dict")
+(ac-config-default)
+
+;; To activate ESS auto-complete for R
+(setq ess-use-auto-complete 'script-only)
+
+;;======================================================================
+;; Functions (for insane things!)
+;;======================================================================
+
+;;----------------------------------------------------------------------
+;; Duplicate lines (like in Geany).
+;; http://stackoverflow.com/questions/88399/how-do-i-duplicate-a-whole-line-in-emacs
+
+(defun duplicate-line ()
+  (interactive)
+  (move-beginning-of-line 1)
+  (kill-line)
+  (yank)
+  (newline)
+  (yank)
+  )
+
+(global-set-key (kbd "\C-c d") 'duplicate-line)
+
+;;----------------------------------------------------------------------
+;; Cut and copy without selection.
+;; http://ergoemacs.org/emacs/emacs_copy_cut_current_line.html
+
+(defun copy-line-or-region ()
+  "Copy current line, or current text selection."
+  (interactive)
+  (if (region-active-p)
+      (kill-ring-save
+       (region-beginning)
+       (region-end)
+       )
+    (kill-ring-save
+     (line-beginning-position)
+     (line-beginning-position 2)
+     )
+    )
+  )
+
+(defun cut-line-or-region ()
+  "Cut the current line, or current text selection."
+  (interactive)
+  (if (region-active-p)
+      (kill-region
+       (region-beginning)
+       (region-end)
+       )
+    (kill-region
+     (line-beginning-position)
+     (line-beginning-position 2)
+     )
+    )
+  )
+
+(global-set-key (kbd "S-<delete>") 'cut-line-or-region)  ; cut.
+(global-set-key (kbd "C-<insert>") 'copy-line-or-region) ; copy.
+
+;;----------------------------------------------------------------------
+;; (un)Comment without selection.
+
+(defun comment-line-or-region ()
+  "Comment or uncomment current line, or current text selection."
+  (interactive)
+  (if (region-active-p)
+      (comment-or-uncomment-region
+       (region-beginning)
+       (region-end)
+       )
+    (comment-or-uncomment-region
+     (line-beginning-position)
+     (line-beginning-position 2)
+     )
+    )
+  )
+
+(global-set-key (kbd "M-;") 'comment-line-or-region)
+
+;;----------------------------------------------------------------------
+;; Move lines.
+;; http://www.emacswiki.org/emacs/MoveLine
+
+(defun move-line (n)
+  "Move the current line up or down by N lines."
+  (interactive "p")
+  (setq col (current-column))
+  (beginning-of-line) (setq start (point))
+  (end-of-line) (forward-char) (setq end (point))
+  (let ((line-text (delete-and-extract-region start end)))
+    (forward-line n)
+    (insert line-text)
+    ;; restore point to original column in moved line
+    (forward-line -1)
+    (forward-char col)))
+
+(defun move-line-up (n)
+  "Move the current line up by N lines."
+  (interactive "p")
+  (move-line (if (null n) -1 (- n))))
+
+(defun move-line-down (n)
+  "Move the current line down by N lines."
+  (interactive "p")
+  (move-line (if (null n) 1 n)))
+
+(global-set-key (kbd "M-<") 'move-line-up)
+(global-set-key (kbd "M->") 'move-line-down)
+
+;;----------------------------------------------------------------------
+;; Move regions.
+
+(defun move-region (start end n)
+  "Move the current region up or down by N lines."
+  (interactive "r\np")
+  (let ((line-text (delete-and-extract-region start end)))
+    (forward-line n)
+    (let ((start (point)))
+      (insert line-text)
+      (setq deactivate-mark nil)
+      (set-mark start))))
+
+(defun move-region-up (start end n)
+  "Move the current line up by N lines."
+  (interactive "r\np")
+  (move-region start end (if (null n) -1 (- n))))
+
+(defun move-region-down (start end n)
+  "Move the current line down by N lines."
+  (interactive "r\np")
+  (move-region start end (if (null n) 1 n)))
+
+(global-set-key (kbd "M-[") 'move-region-up)
+(global-set-key (kbd "M-]") 'move-region-down)
+
+;;----------------------------------------------------------------------
+;; Improved version of occur. Quick navigation.
+;; http://ignaciopp.wordpress.com/2009/06/10/customizing-emacs-occur/
+
+(defun my-occur (&optional arg)
+  "Make sure to always put occur in a vertical split, into a
+   narrower buffer at the side. I didn't like the default
+   horizontal split, nor the way it messes up the arrangement of
+   windows in the frame or the way in which the standard way uses
+   a neighbor window."
+  (interactive "P")
+  ;; store whatever frame configuration we are currently in
+  (window-configuration-to-register ?y)
+  (occur (read-from-minibuffer "Regexp: "))
+  (if (occur-check-existence)
+      (progn
+        (delete-other-windows)
+	;; (split-window-horizontally)
+	;; (enlarge-window-horizontally -30)
+        (split-window-vertically)
+        (enlarge-window -10)
+	;; (set-cursor-color "green")
+        )
+    )
+  (occur-procede-accordingly)
+  (next-error-follow-minor-mode) ;;+
+  )
+
+(defun occur-procede-accordingly ()
+  "Switch to occur buffer or prevent opening of the occur window
+   if no matches occurred."
+  (interactive "P")
+  (if (not(get-buffer "*Occur*"))
+      (message "There are no results.")
+    (switch-to-buffer "*Occur*")))
+
+(defun occur-check-existence()
+  "Signal the existence of an occur buffer depending on the
+   number of matches."
+  (interactive)
+  (if (not(get-buffer "*Occur*")) nil t)
+  )
+
+;; Key binding.
+(define-key global-map (kbd "C-S-o") 'my-occur)
+
+;; http://www.emacswiki.org/emacs/OccurMode
+;; To show more context lines, use
+;; C-U 5 M-x occur regexp-to-search
+
+(defun occur-mode-quit ()
+  "Quit and close occur window. I want to press 'q' and leave
+   things as they were before in regard of the split of windows
+   in the frame. This is the equivalent of pressing C-x 0 and
+   reset windows in the frame, in whatever way they were, plus
+   jumping to the latest position of the cursor which might have
+   been changed by using the links out of any of the matches
+   found in occur."
+  (interactive)
+  (switch-to-buffer "*Occur*")
+  ;; in order to know where we put the cursor they might have jumped from occur
+  (other-window 1)                  ;; go to the main window
+  (point-to-register ?1)            ;; store the latest cursor position
+  (switch-to-buffer "*Occur*")      ;; go back to the occur window
+  (kill-buffer "*Occur*")           ;; delete it
+  (jump-to-register ?y)             ;; reset the original frame state
+  ;; (set-cursor-color "rgb:ff/fb/53") ;; reset cursor color
+  (register-to-point ?1))           ;; re-position cursor
+
+;; Some key bindings defined below. Use "p" ans "n" as in dired mode
+;; (without Cntrl key) for previous and next line; just show occurrence
+;; without leaving the "occur" buffer; use RET to display the line of
+;; the given occurrence, instead of jumping to i,t which you do clicking
+;; instead; also quit mode with Ctrl-g.
+
+(define-key occur-mode-map (kbd "q") 'occur-mode-quit)
+;; (define-key occur-mode-map (kbd "C-RET") 'occur-mode-goto-occurrence-other-window)
+;; (define-key occur-mode-map (kbd "C-<up>") 'occur-mode-goto-occurrence-other-window)
+;; (define-key occur-mode-map (kbd "RET") 'occur-mode-display-occurrence)
+;; (define-key occur-mode-map (kbd "p") 'previous-line)
+;; (define-key occur-mode-map (kbd "n") 'next-line)
+
 
 ;;======================================================================
 ;; end of .emacs
